d18dabf94052c96081dad8744c98ba3b753cab43
StefanTablet <info@stefan-neubig.de>
2015-10-27 19:03:45 +0100

    initial commit

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,14 @@
+
+public class HelloUser {
+
+	private String userName;
+	
+	public HelloUser(String userName) {
+		this.userName = userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello " + userName + "!");
+	}
+	
+}
